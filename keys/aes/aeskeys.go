af1d3dcb55962532d550346324039dec079e90c5
rajnikant <rajnikant12345@gmail.com>
2017-12-12 19:40:43 +0530

    latest

---
+++ keys/aes/aeskeys.go
@@ -0,0 +1,20 @@
+package aes
+
+import "errors"
+
+type Aes struct {
+	name string
+	Size int
+	RawBytes []byte
+}
+
+
+
+func CreateAESKey (name string , size int) (*Aes , error) {
+	var message string
+
+	return nil, errors.New(message)
+}
+
+
+
